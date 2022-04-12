25aeef884dae91d7a57deed05d96478329f73065
cristian navarro leon <cristiannl.0117@gmail.com>
2022-04-12 08:28:00 -0500

    es lint config

--- type-script-base/src/index.ts
+++ type-script-base/src/index.ts
@@ -1,12 +1,12 @@
-import express, { request, response } from 'express'
+import express from 'express';
 
 const app = express();
 const port = 3000;
 
 app.get('/', ( request, response ) => {
-    response.send('Hello my friend')
+    response.send('Hello my friend');
 });
 
 app.listen( port, () => {
-    console.log(`Server listening on ${port}`)
+    console.log(`Server listening on ${port}`);
 });
